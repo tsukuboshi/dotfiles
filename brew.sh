e63288020217f7498bee73b51367247430aa5114
Kaito Yoshiba <silk.120.k6god3@gmail.com>
2021-02-27 23:38:30 +0900

    Add brew bundle command

--- brew.sh
+++ brew.sh
@@ -9,3 +9,5 @@ xcode-select --install > /dev/null
 if ! builtin command -v brew > /dev/null; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+brew bundle
