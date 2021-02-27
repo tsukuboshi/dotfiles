80cb431f44d8750862dd1d45c8c46da6b75c3e8e
Kaito Yoshiba <silk.120.k6god3@gmail.com>
2021-02-27 23:53:31 +0900

    Change comments from Japanese to English

--- default.sh
+++ default.sh
@@ -9,43 +9,43 @@ fi
 # Base
 #----------------------------------------------------------
 
-# 自動大文字の無効化
+# Disable auto-capitalization
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 
 #----------------------------------------------------------
 # Dock
 #----------------------------------------------------------
 
-# アプリケーション起動時のアニメーションを無効化
+# Disable animation at application launch
 defaults write com.apple.dock launchanim -bool false
 
 #----------------------------------------------------------
 # Finder
 #----------------------------------------------------------
 
-# アニメーションを無効化
+# Disable animation
 defaults write com.apple.finder DisableAllAnimations -bool true
 
-# デフォルトで隠しファイルを表示
+# Show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
-# 全ての拡張子のファイルを表示
+# Show files with all extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# ステータスバーを表示
+# Display the status bar
 defaults write com.apple.finder ShowStatusBar -bool true
 
-# パスバーを表示
+# Display the path bar
 defaults write com.apple.finder ShowPathbar -bool true
 
 #----------------------------------------------------------
 # SystemUIServer
 #----------------------------------------------------------
 
-# メニューバーに日付、曜日、24時間、秒数を表示
+# Display date, day, and time in the menu bar
 defaults write com.apple.menuextra.clock DateFormat -string 'EEE d MMM HH:mm'
 
-# メニューバーに割合 (%) を表示
+# Display battery level in the menu bar
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 
 
