cb201c2797326efa2246228b2e29bef5f19e4bbc
Kaito Yoshiba <silk.120.k6god3@gmail.com>
2021-06-20 17:59:53 +0900

    Modify Brewfile settings

--- .bin/brew.sh
+++ .bin/brew.sh
@@ -10,4 +10,4 @@ if ! builtin command -v brew > /dev/null; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew bundle
+brew bundle --global
