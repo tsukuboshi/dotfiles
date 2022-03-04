5e6e0319229cc4ba33754db71ad0559fd637feac
Kaito Yoshiba <silk.120.k6god3@gmail.com>
2022-03-04 09:21:21 +0900

    Change how to install brew command

--- .bin/brew.sh
+++ .bin/brew.sh
@@ -7,7 +7,7 @@ fi
 
 xcode-select --install > /dev/null
 if ! builtin command -v brew > /dev/null; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 brew bundle --global
