84aafc291598f29b17c069905e00056704bb14ac
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2023-05-27 19:04:58 +0900

    Modify how to install extensions

--- vscode/sync.sh
+++ vscode/sync.sh
@@ -10,12 +10,10 @@ fi
 
 # Install extensions using the code command
 if [ "$(which code)" != "" ]; then
-  if [ "$(code --list-extensions)" != "$(cat "${SCRIPT_DIR}/extensions")" ]; then
-    cat < "${SCRIPT_DIR}/extensions" | while read -r line
-    do
-      code --install-extension "$line"
-    done
-  fi
+  cat < "${SCRIPT_DIR}/extensions" | while read -r line
+  do
+    code --install-extension "$line"
+  done
 else
   echo "Install the code command from the command palette to add your extensions."
 fi
