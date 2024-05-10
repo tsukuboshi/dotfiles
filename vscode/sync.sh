82228c9517bacd7aa7b5c49692e58331ddc24414
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2024-05-10 23:20:55 +0900

    Modify vscode sync

--- vscode/sync.sh
+++ vscode/sync.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-VSCODE_SET_DIR="${HOME}/Library/Application Support/Code/User"
 
 # Link settings.json
-if [ -L "${VSCODE_SET_DIR}/settings.json" ]; then
-  ln -fsvn "${SCRIPT_DIR}/settings.json" "${VSCODE_SET_DIR}/settings.json"
+if [ -L "${HOME}/Library/Application\ Support/Code/User/settings.json" ]; then
+  ln -fsvn "${SCRIPT_DIR}/settings.json" "${HOME}/Library/Application\ Support/Code/User/settings.json"
 fi
 
 # Install extensions using the code command
