10bb6a63ad5272a4a3531a015e02465e1fb33e4f
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2025-07-19 21:28:58 +0900

    Modify runtime settings

--- runtime/sync.sh
+++ runtime/sync.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+MISE_SETTING_PATH="${HOME}/.config/mise/config.toml"
 
 # Link .tool-versions
-if [ -L "${HOME}/mise.toml" ]; then
-  ln -fsvn "${SCRIPT_DIR}/mise.toml" "${HOME}/mise.toml"
+if [ -L "${MISE_SETTING_PATH}" ]; then
+  echo "Linking config.toml to mise..."
+  ln -fsvn "${SCRIPT_DIR}/config.toml" "${MISE_SETTING_PATH}"
 fi
 
-# Install asdf plugins
+# Install mise plugins
 if [ "$(which mise)" != "" ]; then
+  echo "Installing mise plugins..."
   mise install
 else
   echo "Install the mise command"
