e585a92a9564585db6a87447ffe0a946b6741a6c
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2025-09-10 20:50:52 +0900

    Modify link

--- aiagent/sync.sh
+++ aiagent/sync.sh
@@ -9,6 +9,6 @@ if [ "$(which claude)" != "" ]; then
   ln -fsvn "${SCRIPT_DIR}/CLAUDE.md" "${CLAUDE_SETTING_PATH}/CLAUDE.md"
   ln -fsvn "${SCRIPT_DIR}/settings.json" "${CLAUDE_SETTING_PATH}/settings.json"
   for file in "${SCRIPT_DIR}"/commands/*; do
-    ln -fsvn "$file" "${CLAUDE_SETTING_PATH}/commands/"
+    ln -fsvn "$file" "${CLAUDE_SETTING_PATH}/commands"
   done
 fi
