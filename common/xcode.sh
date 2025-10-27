975d2993b86fd2a89c066a6f9353b877a32d8eda
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2025-10-27 17:29:48 +0900

    Modify how to setup xcode

--- common/xcode.sh
+++ common/xcode.sh
@@ -7,16 +7,6 @@ fi
 
 if xcode-select -p &>/dev/null; then
 	echo "Xcode Command Line Tools is already installed."
-
-	echo "Checking for updates..."
-	XCODE_UPDATE=$(softwareupdate --list 2>&1 | grep -i "Command Line Tools")
-
-	if [ -n "$XCODE_UPDATE" ]; then
-		echo "Update available. Installing updates..."
-		softwareupdate --install --all
-	else
-		echo "Xcode Command Line Tools is up to date."
-	fi
 else
 	echo "Xcode Command Line Tools is not installed. Installing..."
 	xcode-select --install
