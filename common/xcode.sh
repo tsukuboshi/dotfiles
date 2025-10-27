01ff4a49c09613fbcb83c5e71cd2863bcefac18f
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2025-10-27 17:24:48 +0900

    Modify how to setup xcode

--- common/xcode.sh
+++ common/xcode.sh
@@ -5,4 +5,25 @@ if [ "$(uname)" != "Darwin" ] ; then
 	exit 1
 fi
 
-xcode-select --install > /dev/null
+if xcode-select -p &>/dev/null; then
+	echo "Xcode Command Line Tools is already installed."
+
+	echo "Checking for updates..."
+	XCODE_UPDATE=$(softwareupdate --list 2>&1 | grep -i "Command Line Tools")
+
+	if [ -n "$XCODE_UPDATE" ]; then
+		echo "Update available. Installing updates..."
+		softwareupdate --install --all
+	else
+		echo "Xcode Command Line Tools is up to date."
+	fi
+else
+	echo "Xcode Command Line Tools is not installed. Installing..."
+	xcode-select --install
+
+	echo "Waiting for installation to complete..."
+	until xcode-select -p &>/dev/null; do
+		sleep 5
+	done
+	echo "Installation completed."
+fi
