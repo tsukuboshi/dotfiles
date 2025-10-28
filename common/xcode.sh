42d4e7389a55aed0e2fb2d53293a4769c68febf7
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2025-10-28 18:11:13 +0900

    Add install success judgement

--- common/xcode.sh
+++ common/xcode.sh
@@ -5,7 +5,10 @@ if xcode-select -p &>/dev/null; then
 	printf "\033[1;33m⚠ Xcode Command Line Tools is already installed\033[0m\n"
 else
 	printf "\033[1;36mInstalling Xcode Command Line Tools...\033[0m\n"
-	xcode-select --install
+	xcode-select --install || {
+		printf "\033[1;31m✗ Xcode Command Line Tools installation failed. Exiting...\033[0m\n"
+		exit 1
+	}
 
 	printf "\033[1;36mWaiting for installation to complete...\033[0m\n"
 	until xcode-select -p &>/dev/null; do
