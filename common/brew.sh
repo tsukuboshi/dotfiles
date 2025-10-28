cadf9f235208bce84c8d4ea4989c05feb1a505c6
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2025-10-28 17:12:16 +0900

    Modify mark

--- common/brew.sh
+++ common/brew.sh
@@ -2,7 +2,7 @@
 
 printf "\n\033[1;36m=== Checking Homebrew ===\033[0m\n"
 if command -v brew >/dev/null 2>&1; then
-	printf "\033[1;33m✓ Homebrew is already installed\033[0m\n"
+	printf "\033[1;33m⚠ Homebrew is already installed\033[0m\n"
 else
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
